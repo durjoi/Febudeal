b2c764aa26ac4fd4be7b5b73eb154563336d06fe
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2019-06-22 15:08:45 +0600

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  </head>
+  <body>
+    <div class="menubar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="menubar__logo">
+              <h2>Febudeal</h2>
+            </div>
+          </div>
+          <div class="col-md-5">
+              <form class="menubar__srcbox" action="#" method="#">
+                <input type="text" name="" class="menubar__srcbox-inputfield">
+                <input type="submit" name="" value="Search">
+              </form>
+          </div>
+          <div class="col-md-4">
+            <div class="menubar__menu">
+              <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="#">Cart</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <script type="text/javascript" src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+  </body>
+</html>
