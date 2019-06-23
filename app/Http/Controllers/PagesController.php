ab3705a6dbe1b1e1c22942316da9bcf2b421b344
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2019-06-23 11:26:07 +0600

    layouts added

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,9 +8,9 @@ class PagesController extends Controller
 {
     public function index()
     {
-      return view('layouts.master');
+      return view('pages.home');
     }
-    
+
     public function about()
     {
       return view('pages.about');
