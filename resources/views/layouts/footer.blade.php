46f274913b4fa3f65e73006b61bd68c933414423
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2019-06-24 13:16:22 +0600

    home page banner, products, footer added

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,104 @@
+<!-- Footer Start -->
+<div class="footertop">
+  <div class="ccontainer">
+    <div class="row">
+      <div class="col-2">
+        <div class="footertop__item">
+          <h4>about</h4>
+          <ul>
+            <li> <a href="#">contact us</a> </li>
+            <li> <a href="#">about us</a> </li>
+            <li> <a href="#">Febudeal stories</a> </li>
+            <li> <a href="#">press</a> </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-2">
+        <div class="footertop__item">
+          <h4>help</h4>
+          <ul>
+            <li> <a href="#">payments</a> </li>
+            <li> <a href="#">shipping</a> </li>
+            <li> <a href="#">cancellation &amp; returns</a> </li>
+            <li> <a href="#">faq</a> </li>
+            <li> <a href="#">report infringement</a> </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-2">
+        <div class="footertop__item">
+          <h4>policy</h4>
+          <ul>
+            <li> <a href="#">return policy</a> </li>
+            <li> <a href="#">terms of use</a> </li>
+            <li> <a href="#">security</a> </li>
+            <li> <a href="#">privacy</a> </li>
+            <li> <a href="#">sitemap</a> </li>
+            <li> <a href="#">EPR Compliance</a> </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-2">
+        <div class="footertop__item">
+          <h4>social</h4>
+          <ul>
+            <li> <a href="#">facebook</a> </li>
+            <li> <a href="#">twitter</a> </li>
+            <li> <a href="#">youtube</a> </li>
+          </ul>
+        </div>
+      </div>
+
+
+      <div class="col-2">
+        <div class="footertop__item__info">
+          <h4>mail us</h4>
+          <p>Flipkart Internet Private Limited,
+              Buildings Alyssa, Begonia &
+              Clove Embassy Tech Village,
+              Outer Ring Road, Devarabeesanahalli Village,
+              Bengaluru, 560103,
+              Karnataka, India.
+            </p>
+        </div>
+      </div>
+
+
+      <div class="col-2">
+        <div class="footertop__item__info">
+          <h4>Registered Office Address:</h4>
+          <p>Flipkart Internet Private Limited,
+              Buildings Alyssa, Begonia &
+              Clove Embassy Tech Village,
+              Outer Ring Road, Devarabeesanahalli Village,
+              Bengaluru, 560103,
+              Karnataka, India
+              CIN : U51109KA2012PTC066107
+              Telephone: 1800 208 9898
+            </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-8">
+        <ul>
+          <li> <a href="#">sell on febudeal</a> </li>
+          <li> <a href="#">advertise</a> </li>
+          <li> <a href="#">Help center</a> </li>
+        </ul>
+      </div>
+      <div class="col-md-4 text-center">
+          <p>&copy; 2007-2019 Flipkart.com.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Footer End -->
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,5 +3,446 @@
 @section('content')
   @include('layouts.heroBanner')
   @include('layouts.productSliderOne')
+  <!-- Three banner Start -->
+  <div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+          <div class="bnnrthree__item">
+            <img src={{ asset('images/banner1.jpg') }} alt="">
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="bnnrthree__item">
+            <img src={{ asset('images/banner1.jpg') }} alt="">
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="bnnrthree__item">
+            <img src={{ asset('images/banner1.jpg') }} alt="">
+          </div>
+        </div>
+    </div>
+  </div>
+  <!-- Three Banner End -->
+
   @include('layouts.productSliderTwo')
+
+  <!-- Four Banner Start -->
+  <div class="bnnrfour">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  <!-- Four Banner End -->
+  <!-- Catagory Panel One Start -->
+  <div class="prdcts">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-6">
+              <h2 class="prdcts__title">Deals of the Day</h2>
+          </div>
+          <div class="col-md-6 text-right">
+            <button type="button" class="prdcts__allbtn">View all</button>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="prdcts__slider">
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Catagory Panel One End -->
+  <!-- Catagory Panel One Start -->
+  <div class="prdcts">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-6">
+              <h2 class="prdcts__title">Deals of the Day</h2>
+          </div>
+          <div class="col-md-6 text-right">
+            <button type="button" class="prdcts__allbtn">View all</button>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="prdcts__slider">
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Catagory Panel One End -->
+  <!-- Four Banner Start -->
+  <div class="bnnrfour">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner1.jpg') }} alt="">
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  <!-- Four Banner End -->
+  <!-- Catagory Panel One Start -->
+  <div class="prdcts">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-6">
+              <h2 class="prdcts__title">Deals of the Day</h2>
+          </div>
+          <div class="col-md-6 text-right">
+            <button type="button" class="prdcts__allbtn">View all</button>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="prdcts__slider">
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Catagory Panel One End -->
+  <!-- Catagory Panel One Start -->
+  <div class="prdcts">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-6">
+              <h2 class="prdcts__title">Deals of the Day</h2>
+          </div>
+          <div class="col-md-6 text-right">
+            <button type="button" class="prdcts__allbtn">View all</button>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="prdcts__slider">
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+
+            <div class="prdcts__item text-center">
+              <div class="img__container">
+                <img src={{ asset('images/prdct1.jpg') }} alt="#">
+              </div>
+              <h4>Products Header</h4>
+              <p class="item__price">25,000 BDT</p>
+              <p class="item__saler">Mehedi Hassan Durjoi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Catagory Panel One End -->
+  <!-- Two Banner Start -->
+  <div class="bnnrfour">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-6">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner2.jpg') }} alt="">
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="bnnrthree__item">
+              <img src={{ asset('images/banner2.jpg') }} alt="">
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  <!-- Two Banner End -->
+  @include('layouts.footer')
+
+
 @endsection
