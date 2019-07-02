96fb4dfe0966c13307214493c50b171c6ed49ed1
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2019-07-02 13:50:55 +0600

    Saler Dashboard Added

--- resources/views/saler.blade.php
+++ resources/views/saler.blade.php
@@ -1,23 +1,48 @@
-@extends('layouts.master')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Saler Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in as <strong>SALER</strong>!
-                </div>
-            </div>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Saler Dashboard</title>
+    @include('pertials.styles')
+  </head>
+  <body>
+    <header class="slrdashboard">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 text-right">
+            <ul>
+              <li> <a href="#"><i class="far fa-bell"></i></a> </li>
+              <li> <a href="#"><i class="far fa-comment-dots"></i></a> </li>
+              <li> <a href="#"><i class="fas fa-sign-out-alt"></i></a> </li>
+            </ul>
+          </div>
         </div>
+      </div>
+    </header>
+    <div class="lftsidebar">
+      <div class="lftsidebar__top">
+        <h2>Febudeal</h2>
+        <h3 class="sidebar__slrname">Mehedi Hassan Durjoi</h3>
+        <p class="sidebar__slremail">m.h.durjoi@gmail.com</p>
+        <img class="userimg" src="{{ asset('images/img_avatar.png') }}" alt="">
+
+      </div>
+      <div class="leftsidebar__body">
+        <ul>
+          <li> <a href="#">Dashboard</a> </li>
+          <li> <a href="#">Products</a> </li>
+          <li> <a href="#">Active Orders</a> </li>
+          <li> <a href="#">Profile Update</a></li>
+          <li> <a href="#">Post Ads</a></li>
+        </ul>
+      </div>
+
+      </div>
+
     </div>
-</div>
-@endsection
+
+
+  @include('pertials.scripts')
+  </body>
+</html>
