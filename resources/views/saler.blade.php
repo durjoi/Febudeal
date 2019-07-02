d12a2e9ad058e9c16faa2703fc8a7191fc4bce8c
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2019-07-02 15:34:23 +0600

    Saler Dashboard Product page design added

--- resources/views/saler.blade.php
+++ resources/views/saler.blade.php
@@ -6,8 +6,9 @@
     <title>Saler Dashboard</title>
     @include('pertials.styles')
   </head>
-  <body>
-    <header class="slrdashboard">
+  <body style="background-color: #fff !important">
+
+    <header class="slrdashboard__nav">
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-12 text-right">
@@ -20,6 +21,7 @@
         </div>
       </div>
     </header>
+
     <div class="lftsidebar">
       <div class="lftsidebar__top">
         <h2>Febudeal</h2>
@@ -37,10 +39,151 @@
           <li> <a href="#">Post Ads</a></li>
         </ul>
       </div>
+    </div>
+
+    <div class="slrproducts__top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="slrproducts__heading">Products</p>
+          </div>
+          <div class="col-md-6 text-right">
+            <div class="slrproducts__menu">
+              <ul>
+                <li> <a href="#">Live</a> </li>
+                <li> <a href="#">Pending</a> </li>
+                <li> <a href="#">Upload New</a> </li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
       </div>
-
     </div>
+
+
+    <div class="slr__sectionheading">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3>Your Live Products</h3>
+            <div class="underline"></div>
+            <div class="box"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="slrliveproducts">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Upload Date</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>@fat</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry the Bird</td>
+                  <td>@twitter</td>
+                  <td>@twitter</td>
+                  <td>@twitter</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="slr__sectionheading">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3>Your Pending Products</h3>
+            <div class="underline"></div>
+            <div class="box"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="slrliveproducts">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Upload Date</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>@fat</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry the Bird</td>
+                  <td>@twitter</td>
+                  <td>@twitter</td>
+                  <td>@twitter</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
 
 
   @include('pertials.scripts')
